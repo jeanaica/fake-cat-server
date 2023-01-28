1fe18f90ae1e836a979d1c3ccf0d5561ffd1ed9a
Jeanaica Suplido <sjeanaica@gmail.com>
2023-01-29 05:11:44 +0800

    ⚡️ Move router to plain

--- server.js
+++ server.js
@@ -4,12 +4,7 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
+
 server.use(router);
 server.listen(8000, () => {
   console.log("JSON Server is running");
